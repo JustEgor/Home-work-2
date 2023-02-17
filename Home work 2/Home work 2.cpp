dafb54f01e3896b599338cd815383f48697bfa20
SuperOleg1301 <SuperOleg1301@DESKTOP-R57DN32>
2023-02-17 07:13:46 +0500

    Добавьте файлы проекта.

---
+++ Home work 2/Home work 2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main() {
+
+	//Задание 2.1
+	/*std::cout << "\t  Is it right or worn\n";
+	std::cout << "\t   Try find a place\n";
+	std::cout << "\t  We can all belong?\n";
+	std::cout << "       Be as one tryna get on by\n";
+	std::cout << "\t    Tryna get on by\n";
+	std::cout << "\t      If we unfy\n";
+	std::cout << "\t We should really try\n";
+	std::cout << "\t    All this time...\n\n";
+	std::cout << "\t\t\t\"Sweet Harmony\"-\n";
+	std::cout << "\t\t\tCharles in the Kitchen\n";*/
+
+	//Задание 2.2
+	std::cout << "\t   SELL!!!\n"
+	<< "\tPOTAATOS 100KG\n"
+	<< "    The village of \"Dalnoye\"\n"
+	<< "       +7(922)898-98-98\n";
+
+	return 0;
+
+}
+
